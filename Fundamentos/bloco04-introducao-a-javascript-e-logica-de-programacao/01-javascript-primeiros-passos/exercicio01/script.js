84505c13514971006e7513181a071ea37e5e1c51
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-02-03 15:21:22 -0300

    sobe

---
+++ Fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/exercicio01/script.js
@@ -0,0 +1,12 @@
+const myName = "Luis Cruz";
+const birthCity = "Sorocaba";
+var birthYear = 1987;
+
+console.log(myName, " ", birthCity, " ",birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = Santos;
+console.log(birthCity);
