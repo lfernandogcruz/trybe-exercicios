47b94f18c0385de10c9afe24349d43bcb6d928d9
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-02-03 15:35:24 -0300

    sobe de novo

--- Fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/exercicio01/script.js
+++ Fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/exercicio01/script.js
@@ -1,12 +1,17 @@
-const myName = "Luis Cruz";
-const birthCity = "Sorocaba";
-var birthYear = 1987;
+// const myName = "Luis Cruz";
+// const birthCity = "Sorocaba";
+// var birthYear = 1987;
 
-console.log(myName, " ", birthCity, " ",birthYear);
+// console.log(myName, " ", birthCity, " ",birthYear);
 
-birthYear = 2030;
+// birthYear = 2030;
 
-console.log(birthYear);
+// console.log(birthYear);
 
-birthCity = Santos;
-console.log(birthCity);
+// birthCity = Santos;
+// console.log(birthCity);
+
+
+const squirtle = "melhor pokemon inicial";
+
+console.log(!squirtle); // false
