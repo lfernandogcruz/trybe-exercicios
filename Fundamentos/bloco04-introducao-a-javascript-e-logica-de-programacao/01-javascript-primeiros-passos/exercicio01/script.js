8414b968ae4b24fe811a04b1b2a016122f15a2c0
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-02-04 03:34:08 -0300

    sobe la fi

--- Fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/exercicio01/script.js
+++ Fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/exercicio01/script.js
@@ -1,17 +1,52 @@
 // const myName = "Luis Cruz";
 // const birthCity = "Sorocaba";
 // var birthYear = 1987;
-
+//
 // console.log(myName, " ", birthCity, " ",birthYear);
-
+//
 // birthYear = 2030;
-
+//
 // console.log(birthYear);
-
+//
 // birthCity = Santos;
 // console.log(birthCity);
-
-
-const squirtle = "melhor pokemon inicial";
-
-console.log(!squirtle); // false
+//
+//
+// const squirtle = "melhor pokemon inicial";
+//
+// console.log(!squirtle); // false
+//
+//exercicio 8
+// const primeiroNum = 655;
+// const segundoNum = 3855;
+// const terceiroNum = 38689;
+//
+// if (primeiroNum%2 == 0 || segundoNum%2 == 0 || terceiroNum%2 == 0) {
+//     console.log('um deles é par');
+// } else {
+//     console.log('nenhum é par');
+// }
+//exercicio 9
+// const primeiroNum = 658;
+// const segundoNum = 3858;
+// const terceiroNum = 38680;
+//
+// if (primeiroNum%2 !== 0 || segundoNum%2 !== 0 || terceiroNum%2 !== 0) {
+//     console.log('um deles é impar');
+// } else {
+//     console.log('nenhum é impar');
+// }
+//
+// exercicio 10
+// const custo = 165;
+// const preco = 396;
+// let custoTotal = custo*1.2;
+// let lucro = preco-custoTotal;
+//
+// if (custo < 0 || preco < 0) {
+//     console.log('pane no sistema alguem me desconfigurou');
+// } else {
+//     console.log('O lucro da venda de 1000 unidades é de', (lucro*1000), 'talqueis.');
+// }
+//
+// exercicio 11
