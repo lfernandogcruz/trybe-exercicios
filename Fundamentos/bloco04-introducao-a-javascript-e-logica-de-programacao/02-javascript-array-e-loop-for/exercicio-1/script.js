6afbfb9178c1aa56ebc57f487404208b2ec18237
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-02-03 22:20:10 -0300

    sobe exs 1 ao 8

--- Fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/02-javascript-array-e-loop-for/exercicio-1/script.js
+++ Fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/02-javascript-array-e-loop-for/exercicio-1/script.js
@@ -1,2 +1,73 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//
+//exercicio 1
+// for (let i = 0 ; i < numbers.length ; i += 1) {
+//     console.log(numbers[i]);
+// }
+//
+// exercicio 2
+// let somatoria = 0;
+// for ( let i = 0 ; i < numbers.length ; i +=1) {
+//         somatoria += numbers[i];
+// }
+// console.log(somatoria);
+//
+// exercicio 3
+// let somatoria = 0;
+// for ( let i = 0 ; i < numbers.length ; i +=1) {
+//         somatoria += numbers[i];
+// }
+// let media = somatoria / numbers.length;
+// console.log(media);
+//
+//exercicio 4
+// let somatoria = 0;
+// for ( let i = 0 ; i < numbers.length ; i +=1) {
+//         somatoria += numbers[i];
+// }
+// let media = somatoria / numbers.length;
+// if (media > 20) {
+//     console.log('valor maior que 20');
+// } else {
+//     console.log('valor menor ou igual a 20');
+// }
+//
+// exercicio 5
+// let maiorValor = 0;
+// for (let i = 0; i < numbers.length ; i += 1 ) {
+//     if ( numbers[i] > maiorValor ) {
+//         maiorValor = numbers[i];
+//     }
+// }
+// console.log(maiorValor);
+//
+// exercicio 6
+// let contador = 0;
+// for (let i = 0 ; i < numbers.length ; i += 1 ){
+//     if ( (numbers[i]%2) === 1) {
+//         contador +=1;
+//     }
+// }
+// if (contador != 0) {
+//     console.log('Há ' + contador + ' números ímpares na Array.');
+// } else {
+//     console.log('Nenhum valor ímpar encontrado');
+// }
+//
+// exercicio 7
+// let menorValor = numbers[0];
+// for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] < menorValor) {
+//         menorValor = numbers[i];
+//     }
+// }
+// console.log('O menor valor é ' + menorValor);
+//
+// exercicio 8
+// let contando = [];
+// for ( let i = 1 ; i <= 25 ; i += 1 ) {
+//     contando.push(i)
+// }
+// console.log(contando);
+//
+//exercicio 9
