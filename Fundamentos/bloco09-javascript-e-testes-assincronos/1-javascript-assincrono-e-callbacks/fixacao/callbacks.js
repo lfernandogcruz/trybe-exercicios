88838d5567e042c5a9f8d12be6eaa852bee05f1b
LuisCruz <lfernandogcruz@yahoo.com.br>
2022-03-28 16:09:11 -0300

    fix bl 9 fora da branch por vacilo

--- Fundamentos/bloco09-javascript-e-testes-assincronos/1-javascript-assincrono-e-callbacks/fixacao/callbacks.js
+++ Fundamentos/bloco09-javascript-e-testes-assincronos/1-javascript-assincrono-e-callbacks/fixacao/callbacks.js
@@ -1,4 +1,5 @@
 // 1.
+// 1.1.
 // Adicione uma callback como parâmetro da funcão getUser .
 // No código abaixo você tem a função getUser , que define um objeto com os dados de uma pessoa.
 // Complete a função getUser de forma que ela receba uma função callback como parâmetro
@@ -23,7 +24,7 @@
 // console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
 // console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
 
-// 2.
+// 1.2.
 // Passe, como parâmetro e como retorno, uma callback para a função getUser .
 // No código abaixo você tem a função getUser modificada, que agora funciona de modo assíncrono
 // e imprime dados de uma pessoa depois de um certo tempo. Complete a função getUser de forma
@@ -32,23 +33,23 @@
 // - Insira uma callback como parâmetro da função getUser ;
 // - Retorne a callback passada como parâmetro na função getUser ;
 
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+// const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
 
-const getUser = (callback) => {
-  setTimeout(() => {
-    const user = {
-      firstName: "Ivan",
-      lastName: "Ivanovich",
-      nationality: "Russian",
-    };
-    // Retorne a `callback` passada como parâmetro na função `getUser`
-    // Dica: você pode manter o `console.log()`
-    console.log(callback(user));
-  }, delay());
-};
+// const getUser = (callback) => {
+//   setTimeout(() => {
+//     const user = {
+//       firstName: "Ivan",
+//       lastName: "Ivanovich",
+//       nationality: "Russian",
+//     };
+//     // Retorne a `callback` passada como parâmetro na função `getUser`
+//     // Dica: você pode manter o `console.log()`
+//     console.log(callback(user));
+//   }, delay(2000));
+// };
 
-getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
-getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
+// getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+// getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo
